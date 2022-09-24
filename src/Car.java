35d61b0e882b00ac58133cc6e27f79e8b9f8a78d
Tobias Sörensson <tobbesoren@gmail.com>
2022-09-24 11:00:33 +0200

    Sorry, here is the working version.

--- src/Car.java
+++ src/Car.java
@@ -1,14 +1,57 @@
-public class Car extends Vehicle{
+public class Car implements Vehicle {
+
+    private Driver driver;
+    private int toGo;
+    private final int driveDistance;
+    private final int ageLimit;
+
 
     public Car () {
 
-        super();
-        this.setToGo(100);
-        this.setAgeLimit(18);
-        this.setDriveDistance(10);
-        this.setVehicleType("Car");
+        this.toGo = 100;
+        this.ageLimit = 18;
+        this.driveDistance = 10;
 
         System.out.println("Car created. " + this.getToGo() + " miles to go!");
     }
 
+    public int getToGo() {
+        return toGo;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public int getDriveDistance() {
+        return driveDistance;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setDriver(Driver driver) {
+
+        if(driver.getAge() >= ageLimit) {
+            this.driver = driver;
+            System.out.println("Driver changed to " + driver.getName());
+        } else {
+            System.out.println("Driver not changed! " + driver.getName() + " is " + driver.getAge() + ", but must be "
+                    + ageLimit + " or older to drive car");
+        }
+    }
+
+    public void drive () {
+        if (this.driver == null) {
+            System.out.println("Car didn’t drive - there’s no driver!");
+            return;
+        }
+        if (toGo - driveDistance >= 0) {
+            toGo -= driveDistance;
+            System.out.println("Car drove " + driveDistance + " miles - " + toGo + " miles to go");
+        } else {
+            System.out.println("Car won't drive any longer");
+        }
+    }
 }
--- src/MilitaryTank.java
+++ src/MilitaryTank.java
@@ -1,13 +1,57 @@
-public class MilitaryTank extends Vehicle{
+public class MilitaryTank implements Vehicle {
+
+    private Driver driver;
+    private int toGo;
+    private final int driveDistance;
+    private final int ageLimit;
+
+
     public MilitaryTank () {
 
-        super();
-        this.setToGo(2000);
-        this.setAgeLimit(25);
-        this.setDriveDistance(5);
-        this.setVehicleType("Tank");
+        this.toGo = 2000;
+        this.ageLimit = 25;
+        this.driveDistance = 5;
 
-        System.out.println("Tank created. " + this.getToGo() + " miles to go!");
+        System.out.println("Tank created. " + toGo + " miles to go!");
     }
 
+    public int getToGo() {
+        return toGo;
+    }
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public int getDriveDistance() {
+        return driveDistance;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setDriver(Driver driver) {
+
+        if(driver.getAge() >= ageLimit) {
+            this.driver = driver;
+            System.out.println("Driver changed to " + driver.getName());
+        } else {
+            System.out.println("Driver not changed! " + driver.getName() + " is " + driver.getAge() + ", but must be "
+                    + ageLimit + " or older to drive tank");
+        }
+    }
+
+    public void drive () {
+        if (this.driver == null) {
+            System.out.println("Tank didn’t drive - there’s no driver!");
+            return;
+        }
+        if (toGo - driveDistance >= 0) {
+            toGo -= driveDistance;
+            System.out.println("Tank drove " + driveDistance + " miles - " + toGo + " miles to go");
+        } else {
+            System.out.println("Tank won't drive any longer");
+        }
+    }
 }
